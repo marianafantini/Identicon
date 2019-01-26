22a02e57dc54cfce56f70735560167d302bc3cc0
mariana-fantini-movile <mariana.fantini@wavy.global>
2019-01-26 17:40:06 -0200

    Removed commented code

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -38,10 +38,7 @@ defmodule Identicon do
   end
 
   def mirror_row(row) do
-    # input: [145, 46, 200]
     [first, second | _tail] = row
-
-    # output: [145, 46, 200, 46, 145]
     row ++ [second, first]
   end
 
@@ -51,18 +48,6 @@ defmodule Identicon do
   def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
     %Identicon.Image{image | color: {red, green, blue}}
   end
-
-  ### first version of pick_color: 
-  # refactor: it is possible to use pattern matching as we pass an argument to the function
-  # def pick_color(image) do
-  #   %Identicon.Image{hex: [red, green, blue | _tail]} = image
-    
-  #   # adding a color value to this struct
-  #   %Identicon.Image{image | color: {red, green, blue}}
-
-  #   # changed from list to tuple because lists have elements with no special meaning,
-  #   # tuples are used for data with special meaning (first is red, second is green, third is blue)
-  # end
   
   @doc """
     Received an input and returns its hash as a list of numbers
